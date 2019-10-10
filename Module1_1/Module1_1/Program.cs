c6d8f913fb79807ef3d11e4e477edd62448689b3
NumbreZoziron <44519785+NumbreZoziron@users.noreply.github.com>
2019-10-10 15:04:53 +0300

    Module1_1
    New project was created. Task 1 was completed.

---
+++ Module1_1/Module1_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two numbers: ");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            a = a + b - (b = a);
+            Console.WriteLine(a + ";" + b);
+            Console.ReadKey();
+        }
+    }
+}
